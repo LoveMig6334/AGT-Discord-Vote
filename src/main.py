4052b0fa95899c8d69f154d4384bb33c32a7eb4a
Thatt <tom.tom.thanet@gmail.com>
2025-06-25 20:31:48 +0700

    Refactor main.py to initialize logging and Discord bot intents

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import logging
+import os
+
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+discord_token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.moderation = True
 
 
-def main() -> None:
-    print(discord.__version__)
-
-
-if __name__ == "__main__":
-    main()
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
